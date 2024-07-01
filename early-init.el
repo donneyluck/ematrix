286fec7cb436cdcb1661b2ceb118a13ed5e82252
donneyluck <donney.luck@gmail.com>
2024-07-01 14:18:51 +0800

    add(file): add early-init.el

---
+++ early-init.el
@@ -0,0 +1,70 @@
+;;; early-init.el --- Ematrix early initialization tweaks -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024-2024  MachineSudio
+
+;; Author: donney.luck@gmail.com
+
+;; ___________               __         .__
+;; \_   _____/ _____ _____ _/  |________|__|__  ___
+;;  |    __)_ /     \\__  \\   __\_  __ \  \  \/  /
+;;  |        \  Y Y  \/ __ \|  |  |  | \/  |>    <
+;; /_______  /__|_|  (____  /__|  |__|  |__/__/\_ \
+;;         \/      \/     \/                     \/
+;;
+;; MINIMALIST & LIGHTWEIGHT EMACS CONFIGURATION FRAMEWORK
+;;                          donneyluck.github.io/ematrix
+
+;;; Commentary:
+
+;;; Code:
+
+(setq
+ ;; Do not make installed packages available when Emacs starts (we use `straight')
+ package-enable-at-startup nil
+ ;; Avoid garbage collections in the startup phase, this will be overwritten by the `me-gc' core module
+ gc-cons-threshold most-positive-fixnum
+ ;; Prefer loading newer files
+ load-prefer-newer t
+ ;; Remove some unneeded UI elements
+ default-frame-alist '((tool-bar-lines . 0)
+                       (menu-bar-lines . 0)
+                       (vertical-scroll-bars)
+                       (left-fringe . 8)
+                       (right-fringe . 13)
+                       (internal-border-width . 15)
+                       (mouse-color . "blue")
+                       (fullscreen . maximized))
+ ;; Explicitly set modes disabled in `default-frame-alist' to nil
+ tool-bar-mode nil
+ menu-bar-mode nil
+ scroll-bar-mode nil
+ ;; Set mode-line format to prevent it from showing at startup
+ mode-line-format nil)
+
+;; It seems like, even when `tool-bar-mode' is nil, `tool-bar-setup' still be called
+(advice-add 'tool-bar-setup :override #'ignore)
+
+;; NOTE: In Emacs29+, frames can have a transparent background via the
+;; `alpha-background' parameter. For a better experience, this value should be
+;; set early before any frame gets created (i.e. in "early-init.el"). MinEmacs
+;; uses the `$EMATRIX_ALPHA` environment variable that can be set to an integer
+;; value in the [1-100] range (the alpha percentage). When this variable is not
+;; set, Emacs will load the default GUI (without background alpha), and when it
+;; is set but the value is not valid, MinEmacs will fallback to the default
+;; alpha of 93%.
+(when (>= emacs-major-version 29)
+  (when-let* ((alpha (getenv "EMATRIX_ALPHA"))
+              (alpha (string-to-number alpha)))
+    (push `(alpha-background . ,(if (or (zerop alpha) (> alpha 100)) 93 alpha)) default-frame-alist)))
+
+;; Load MinEmacs variables from the `me-vars' core module.
+(load (expand-file-name "core/me-vars.el" (file-name-directory (file-truename load-file-name))) nil t)
+
+;; Better titlebar on MacOS!
+(when (and os/mac (featurep 'ns))
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
+
+;; Load the user early configuration files
+(+load-user-configs 'early-config 'local/early-config)
+
+;;; early-init.el ends here
